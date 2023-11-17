86f45238d77bedbec764f5891963308c4da96f64
bullie_hu <bullie_hu@naver.com>
2023-11-17 16:48:33 +0900

    file upload complete
    Former-commit-id: 5b39896d97fc71a2171c4d15a89c532ec74cfd19

--- BackEnd/WhereIsMyHome_BackEnd/src/main/java/com/ssafy/board/model/service/BoardService.java
+++ BackEnd/WhereIsMyHome_BackEnd/src/main/java/com/ssafy/board/model/service/BoardService.java
@@ -3,13 +3,14 @@ package com.ssafy.board.model.service;
 import java.util.List;
 import java.util.Map;
 
+import com.ssafy.apt.model.ImgInfoDto;
 import com.ssafy.board.model.BoardCommentDto;
 import com.ssafy.board.model.BoardDto;
 import com.ssafy.util.PageNavigation;
 
 public interface BoardService {
 	
-	int writeArticle(BoardDto boardDto) throws Exception;
+	void writeArticle(BoardDto boardDto) throws Exception;
 	List<BoardDto> listArticle(Map<String, String> map, String sortCondition) throws Exception;
 	PageNavigation makePageNavigation(Map<String, String> map) throws Exception;
 	BoardDto getArticle(int articleNo) throws Exception;
