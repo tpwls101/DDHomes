50323cd44fe3f6824a428d1baab567bea94b005a
bullie_hu <bullie_hu@naver.com>
2023-11-20 11:53:17 +0900

    board image delete done
    Former-commit-id: 311fcae103d0b6f99b6272e76fa8f7e6526f52e8

--- FrontEnd/whereismyhome-frontend/src/components/board/BoardModify.vue
+++ FrontEnd/whereismyhome-frontend/src/components/board/BoardModify.vue
@@ -8,7 +8,7 @@ const route = useRoute();
 
 const { articleNo } = route.params;
 
-const article = ref({});
+const boardDto = ref({});
 
 onMounted(() => {
   getArticle();
@@ -21,7 +21,7 @@ const getArticle = () => {
     articleNo,
     ({ data }) => {
       // console.log(data);
-      article.value = data;
+      boardDto.value = data;
     },
     (error) => {
       console.log(error);
@@ -32,25 +32,25 @@ const getArticle = () => {
 // 글 수정
 const ArticleModify = () => {
   // 입력한 데이터 체크
-  if (article.value.userId === "") {
+  if (boardDto.value.userId === "") {
     alert("[오류] 사용자가 확인되지 않습니다. 로그인 해 주세요!");
     return;
-  } else if (article.value.subject === "") {
+  } else if (boardDto.value.subject === "") {
     alert("[오류] 제목을 입력 해 주세요!");
     return;
-  } else if (article.value.content === "") {
+  } else if (boardDto.value.content === "") {
     alert("[오류] 내용을 입력 해 주세요!");
     return;
-  } else if (article.value.boardType === "") {
+  } else if (boardDto.value.boardType === "") {
     alert("[오류] 게시판 종류를 선택해 주세요!");
     return;
   }
 
   modifyArticle(
-    article.value,
+    boardDto.value,
     ({ data }) => {
       console.log(data);
-      let articleNo = article.articleNo;
+      let articleNo = boardDto.articleNo;
       router.push({ name: "board-detail", params: { articleNo } });
     },
     (error) => {
@@ -74,53 +74,61 @@ function boardList(boardType) {
     </div>
     <div class="col-lg-8 col-md-10 col-sm-12">
       <form id="form-modify" method="POST" action="">
-        <div>
-          <label for="boardType" class="form-label">{{ article.boardType }}</label>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="radio" name="boardType" id="announcement" value="announcement"
+            v-model="boardDto.boardType" />
+          <label class="form-check-label" for="inlineRadio1">공지사항</label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="radio" name="boardType" id="information" value="information"
+            v-model="boardDto.boardType" />
+          <label class="form-check-label" for="inlineRadio2">정보</label>
         </div>
         <div class="mb-3">
           <label for="subject" class="form-label">제목 : </label>
-          <input
-            type="text"
-            class="form-control"
-            id="subject"
-            name="subject"
-            v-model="article.subject"
-          />
+          <input type="text" class="form-control" id="subject" name="subject" v-model="boardDto.subject" />
         </div>
         <div class="mb-3">
           <label for="content" class="form-label">내용 : </label>
-          <textarea
-            class="form-control"
-            id="content"
-            name="content"
-            rows="7"
-            v-model="article.content"
-          ></textarea>
+          <textarea class="form-control" id="content" name="content" rows="7" v-model="boardDto.content"></textarea>
+        </div>
+        <div>
+          <p>첨부된 이미지</p>
+          <div class="imginfo">
+            파일1<input class="btn-delete-img" type="button" value="삭제">
+          </div>
+          <div class="imginfo">
+            파일2<input class="btn-delete-img" type="button" value="삭제">
+          </div>
         </div>
         <div class="col-auto text-center">
-          <button
-            type="button"
-            id="btn-modify"
-            class="btn btn-outline-primary mb-3"
-            @click="ArticleModify"
-          >
+          <button type="button" id="btn-modify" class="btn btn-outline-primary mb-3" @click="ArticleModify">
             글수정
           </button>
-          <button
-            type="button"
-            id="btn-list"
-            class="btn btn-outline-danger mb-3"
-            @click="boardList(article.boardType)"
-          >
+          <button type="button" id="btn-list" class="btn btn-outline-danger mb-3" @click="boardList(boardDto.boardType)">
             목록으로이동...
           </button>
         </div>
 
-        <input type="hidden" name="articleNo" value="${article.articleNo}" />
-        <input type="hidden" name="boardType" value="${article.boardType}" />
+        <input type="hidden" name="articleNo" value="${boardDto.articleNo}" />
+        <input type="hidden" name="boardType" value="${boardDto.boardType}" />
       </form>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#content {
+  resize: none;
+}
+
+.imginfo {
+  border: 1px solid black;
+  height: 32px;
+  margin: 10px;
+}
+
+.btn-delete-img {
+  float: right;
+}
+</style>
