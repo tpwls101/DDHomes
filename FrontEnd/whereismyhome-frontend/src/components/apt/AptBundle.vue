b7f751914d851fbbe4269415b9eecee85defbd75
tpwls101 <tpwls101@naver.com>
2023-11-21 17:58:43 +0900

    update
    Former-commit-id: 5c53160ba5a7ce60c65a25e4c3e91ff6f57decac

--- FrontEnd/whereismyhome-frontend/src/components/apt/AptBundle.vue
+++ FrontEnd/whereismyhome-frontend/src/components/apt/AptBundle.vue
@@ -1,12 +1,28 @@
 <script setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+import { useAptStore } from "../../stores/apt";
+import { storeToRefs } from "pinia";
 import AptList from "@/components/apt/AptList.vue";
 import AptMap from "@/components/apt/AptMap.vue";
 import AptDetail from "@/components/apt/AptDetail.vue";
+
+// const route = useRoute();
+
+// const dongCode = ref(route.params.value);
+// console.log("dongCode : " + dongCode.value);
+
+const aptStore = useAptStore();
+
+const { dongCode } = storeToRefs(aptStore);
+
+console.log(dongCode);
 </script>
 
 <template>
   <div>
     <h1>AptBundle</h1>
+    <div></div>
     <div>
       <AptList />
     </div>
@@ -15,7 +31,8 @@ import AptDetail from "@/components/apt/AptDetail.vue";
     </div>
     <div>
       <AptDetail />
+    </div>
   </div>
-</div></template>
+</template>
 
 <style scoped></style>
