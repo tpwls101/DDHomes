c1a195c5905b22334ed8009e4fb02051e08def53
bullie_hu <bullie_hu@naver.com>
2023-11-22 02:10:21 +0900

    forsale regist
    Former-commit-id: 3d82f966599bcfed3d6e2c4d1d4439f55f0b4db9

--- FrontEnd/whereismyhome-frontend/src/components/forsale/ForSaleRegist.vue
+++ FrontEnd/whereismyhome-frontend/src/components/forsale/ForSaleRegist.vue
@@ -16,6 +16,9 @@ const forsaleDto = ref({
   price: "",
 });
 
+// 가격 입력 시 단위 표시해주는 데이터
+const unitPrice = ref("");
+
 // 아파트 검색시 파라미터로 넘길 반응형 데이터
 const dongName = ref("");
 const apartmentName = ref("");
@@ -24,16 +27,47 @@ const apartmentName = ref("");
 const searchResult = ref([]);
 const avgDealAmounMsg = ref("아파트를 선택하면 최근 거래가가 표시돼요!");
 
+// 아파트 검색 결과 아이템 선택 시 클래스 변경 위한 변수
+const prevSelectedItemId = ref("");
+
 onMounted(() => {
   if (userInfo.value != null) {
     forsaleDto.value.userId = userInfo.value.userId;
   }
 });
 
+// 가격 입력 시 작동하는 이벤트
+function checkUnit() {
+  if (forsaleDto.value.price === "") {
+    unitPrice.value = "";
+  } else {
+    // console.log(forsaleDto.value.price.length);
+    let temp = forsaleDto.value.price;
+    console.log(temp);
+    unitPrice.value = forsaleDto.value.price + "원";
+  }
+}
+
 // 등록버튼 입력값 체크
+function isValidInput() {
+  if (forsaleDto.value.aptCode === "") {
+    alert("[오류] 아파트를 선택해 주세요.");
+    return false;
+  } else if (forsaleDto.value.price === "") {
+    alert("[오류] 가격을 입력해 주세요.");
+    return false;
+  } else if (document.querySelector("#upload-img").files.length === 0) {
+    alert("[오류] 사진은 한 장 이상 등록해 주세요.");
+    return false;
+  }
+  return true;
+}
 
 // 매물 등록 버튼
 function registForsaleBtnClicked() {
+  if (!isValidInput()) {
+    return;
+  }
   // 파일 확인
   const imgInfos = document.querySelector("#upload-img").files;
 
@@ -98,6 +132,17 @@ function selectApt(aptCode) {
   );
 
   forsaleDto.value.aptCode = aptCode;
+
+  // 선택된 아이템 배경색 바꾸기
+  for (let i = 0; i < searchResult.value.length; i++) {
+    if (searchResult.value[i].aptCode == aptCode) {
+      document.getElementById(aptCode).classList.add("selected-item");
+    }
+  }
+  if (prevSelectedItemId.value != "") {
+    document.getElementById(prevSelectedItemId.value).classList.remove("selected-item");
+  }
+  prevSelectedItemId.value = aptCode;
 }
 </script>
 
@@ -140,9 +185,10 @@ function selectApt(aptCode) {
               name="price"
               placeholder="가격을 입력하세요!"
               v-model="forsaleDto.price"
+              @keyup="checkUnit"
             />
             <p v-text="avgDealAmounMsg"></p>
-            <p v-show="forsaleDto.price != 0">zz</p>
+            <p v-text="unitPrice"></p>
           </div>
           <div>
             <p>이미지 :</p>
@@ -167,7 +213,6 @@ function selectApt(aptCode) {
       <hr />
       <form id="form-search" method="POST" action="" onsubmit="return false;">
         <div class="search-box">
-          <!-- <div> -->
           <input
             type="text"
             class="form-control"
@@ -184,7 +229,7 @@ function selectApt(aptCode) {
             placeholder="아파트명 입력"
             v-model="apartmentName"
           />
-          <!-- </div> -->
+
           <button
             type="button"
             class="regist-search-btn btn btn-outline-success"
@@ -198,17 +243,22 @@ function selectApt(aptCode) {
         <div
           v-for="item in searchResult"
           :key="item.aptCode"
+          :id="item.aptCode"
           class="result-item"
           @click="selectApt(item.aptCode)"
         >
-          <ul>
-            <li>아파트명: {{ item.apartmentName }}</li>
-            <li>
-              지번주소: {{ item.sidoName }} {{ item.gugunName }} {{ item.dong }} {{ item.jibun }}
-            </li>
-            <li>도로명주소: {{ item.roadName }} {{ item.roadNameBonBun }}</li>
-            <li>준공년도: {{ item.buildYear }}</li>
-          </ul>
+          <p>
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
+              <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+              <path
+                d="M64 48c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16h80V400c0-26.5 21.5-48 48-48s48 21.5 48 48v64h80c8.8 0 16-7.2 16-16V64c0-8.8-7.2-16-16-16H64zM0 64C0 28.7 28.7 0 64 0H320c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm88 40c0-8.8 7.2-16 16-16h48c8.8 0 16 7.2 16 16v48c0 8.8-7.2 16-16 16H104c-8.8 0-16-7.2-16-16V104zM232 88h48c8.8 0 16 7.2 16 16v48c0 8.8-7.2 16-16 16H232c-8.8 0-16-7.2-16-16V104c0-8.8 7.2-16 16-16zM88 232c0-8.8 7.2-16 16-16h48c8.8 0 16 7.2 16 16v48c0 8.8-7.2 16-16 16H104c-8.8 0-16-7.2-16-16V232zm144-16h48c8.8 0 16 7.2 16 16v48c0 8.8-7.2 16-16 16H232c-8.8 0-16-7.2-16-16V232c0-8.8 7.2-16 16-16z"
+              />
+            </svg>
+            아파트명: {{ item.apartmentName }}
+          </p>
+          <p>지번주소: {{ item.sidoName }} {{ item.gugunName }} {{ item.dong }} {{ item.jibun }}</p>
+          <p>도로명주소: {{ item.roadName }} {{ item.roadNameBonBun }}</p>
+          <p>준공년도: {{ item.buildYear }}</p>
         </div>
       </div>
     </div>
@@ -259,6 +309,13 @@ input[type="number"]::-webkit-inner-spin-button {
 
 .result-item {
   border: 1px solid lightgray;
+  border-radius: 5px;
+  margin: 5px;
+  padding: 5px;
+}
+
+.selected-item {
+  background-color: lightgray;
 }
 
 .result-item > * {
