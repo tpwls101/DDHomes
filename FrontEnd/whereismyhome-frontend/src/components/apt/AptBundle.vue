10759bb196ea183d9bdf283686f3908ee2aaa13d
tpwls101 <tpwls101@gmail.com>
2023-11-22 02:13:05 +0900

    map ing
    Former-commit-id: bf11e7d545d752cc57e5acdf90e672b095b83807

--- FrontEnd/whereismyhome-frontend/src/components/apt/AptBundle.vue
+++ FrontEnd/whereismyhome-frontend/src/components/apt/AptBundle.vue
@@ -1,16 +1,9 @@
 <script setup>
-import { ref } from "vue";
-import { useRoute } from "vue-router";
 import { useAptStore } from "../../stores/apt";
 import { storeToRefs } from "pinia";
 import AptList from "@/components/apt/AptList.vue";
 import AptMap from "@/components/apt/AptMap.vue";
 import AptDetail from "@/components/apt/AptDetail.vue";
-
-// const route = useRoute();
-
-// const dongCode = ref(route.params.value);
-// console.log("dongCode : " + dongCode.value);
 
 const aptStore = useAptStore();
 
@@ -32,6 +25,5 @@ console.log(dongCode);
   display: flex;
   flex-direction: row;
   height: 600px;
-  background-color: blue;
 }
 </style>
